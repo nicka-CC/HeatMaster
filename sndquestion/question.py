d0f183e37fc4a72481aabd0d1d1de20a0507f90a
nikita <im.nicka.cc@gmail.com>
2025-03-04 16:24:37 +0300

    init django

--- sndquestion/question.py
+++ sndquestion/question.py
@@ -1,8 +1,23 @@
-a = int(input("Введите длину первого катета (см): "))
-b = int(input("Введите длину второго катета (см): "))
+while True:
+    try:
+        a = int(input("Введите длину первого катета (см): "))
+        break
+    except ValueError:
+        print("Ошибка: Пожалуйста, введите целое число.")
+
+while True:
+    try:
+        b = int(input("Введите длину второго катета (см): "))
+        break
+    except ValueError:
+        print("Ошибка: Пожалуйста, введите целое число.")
+# a = int(input("Введите длину первого катета (см): "))
+# b = int(input("Введите длину второго катета (см): "))
+
 c = (a * a + b * b) ** 0.5
 perimetr = c + a + b
 ploshad = a * b / 2
+
 print(f"Гипотенуза = {c:.0f}")
 print(f"Периметр = {perimetr:.0f}")
 print(f"Площадь = {ploshad:.0f}" )
