9e75785f3ab56737ccf965e077c647248d704c31
NikitaBerdashev <im.nicka.cc@gmail.com>
2025-12-04 10:49:10 +0300

    fix(ui): cart

--- HeatMaster/HeatMaster/settings.py
+++ HeatMaster/HeatMaster/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.43.237','127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "HeatMaster Admin",
+    "site_header": "HeatMaster",
+    "welcome_sign": "Welcome to the HeatMaster admin",
+}
